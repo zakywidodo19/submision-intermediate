2ec0a4d98384bae2607a0c65ddeac6ca43013e61
zakywidodo19 <168552959+zakywidodo19@users.noreply.github.com>
2026-05-21 12:06:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/submision-intermediate/',
+
   root: resolve(__dirname, 'src'),
+
   publicDir: resolve(__dirname, 'src', 'public'),
+
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
+
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
